aca3f484cbcd7ba3552f0aa19d8f631064dcab06
Riagms <riyariya44@gmail.com>
2013-06-20 12:10:47 +0530

    displayed lead information

---
+++ Infoleads.m
@@ -0,0 +1,13 @@
+//
+//  Infoleads.m
+//  Newproject
+//
+//  Created by GMSIndia1 on 6/20/13.
+//  Copyright (c) 2013 GMSIndia1. All rights reserved.
+//
+
+#import "Infoleads.h"
+
+@implementation Infoleads
+
+@end
