9c6057fa3cf7a24273ff236c949528fe8dc50428
Riagms <riyariya44@gmail.com>
2013-06-21 13:38:30 +0530

    cmtd

---
+++ Newproject/CommentsViewController.m
@@ -0,0 +1,38 @@
+//
+//  CommentsViewController.m
+//  Newproject
+//
+//  Created by GMSIndia1 on 6/21/13.
+//  Copyright (c) 2013 GMSIndia1. All rights reserved.
+//
+
+#import "CommentsViewController.h"
+
+@interface CommentsViewController ()
+
+@end
+
+@implementation CommentsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
