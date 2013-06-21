c172ec725aebece806d69dda4eed3c5f2fc12608
GMSIndia 2 <rukminikr@gmsindia-2.mshome.net>
2013-06-21 15:32:08 +0530

    c

--- Newproject/CommentsViewController.h
+++ Newproject/CommentsViewController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface CommentsViewController : UIViewController
-
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollview;
+@property(strong,nonatomic)IBOutlet UITableView *commentTable;
+@property(strong,nonatomic)IBOutlet UITableViewCell *commentcell;
+@property(strong,nonatomic)IBOutlet UITextView *cmntTextView;
+@property(strong,nonatomic)IBOutlet UITextView *commenttextfortable;
+-(IBAction)saveComment:(id)sender;
+-(IBAction)cancelcomment:(id)sender;
+@property(strong,nonatomic)NSMutableArray *commentArray;
 @end
--- Newproject/CommentsViewController.m
+++ Newproject/CommentsViewController.m
@@ -26,6 +26,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _scrollview.frame=CGRectMake(0, 0,1004, 768);
+    [_scrollview setContentSize:CGSizeMake(1004,850)];
+    _commentArray=[[NSMutableArray alloc]init];
+
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -33,6 +37,51 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    // Return the number of rows in the section.
+    return [_commentArray count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"mycell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        [[NSBundle mainBundle]loadNibNamed:@"newCommentCell" owner:self options:nil];
+        cell=_commentcell;
+        
+        
+    }
+    cell.textLabel.text=[_commentArray objectAtIndex:indexPath.row];
+    return cell;
+    
+    
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+
+-(IBAction)saveComment:(id)sender
+{
+    [_commentArray addObject:_cmntTextView.text];
+    [_commentTable reloadData];
+}
+-(IBAction)cancelcomment:(id)sender
+{
+    _cmntTextView.text=@"";
 }
 
 @end
