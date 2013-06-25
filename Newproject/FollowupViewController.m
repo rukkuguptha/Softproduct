6a956d932c88362217f344f9045c8bab0ac42085
Riagms <riyariya44@gmail.com>
2013-06-25 15:59:31 +0530

    cmtd

--- Newproject/FollowupViewController.m
+++ Newproject/FollowupViewController.m
@@ -450,64 +450,64 @@
 }
 
 
--(void)Editfollowup{
-    recordResults = FALSE;
-    NSString *soapMessage;
-  //  NSInteger followupID=0;
-    soapMessage = [NSString stringWithFormat:
-                   
-                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
-                   
-                   
-                   "<soap:Body>\n"
-                   
-                   "<SaveFollowup xmlns=\"http://webserv.kontract360.com/\">\n"
-                   "<HeadLine>%@</HeadLine>\n"
-                   "<LeadLink>%d</LeadLink>\n"
-                   "<OrgContact>%@</OrgContact>\n"
-                   "<CommunicationType>%@</CommunicationType>\n"
-                   "<DateContact>%@</DateContact>\n"
-                   "<TimeContact>%@</TimeContact>\n"
-                   "<Summary>%@</Summary>\n"
-                   "<activityId>%d</activityId>\n"
-                   "<followupid>%d</followupid>\n"
-                   "</SaveFollowup>\n"
-                   "</soap:Body>\n"
-                   "</soap:Envelope>\n",,_ActivityId,followupID];
-    NSLog(@"soapmsg%@",soapMessage);
-    
-    
-    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
-    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
-    
-    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
-    
-    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
-    
-    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    
-    [theRequest addValue: @"http://webserv.kontract360.com/SaveFollowup" forHTTPHeaderField:@"Soapaction"];
-    
-    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
-    [theRequest setHTTPMethod:@"POST"];
-    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    
-    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
-    
-    if( theConnection )
-    {
-        _webData = [NSMutableData data];
-    }
-    else
-    {
-        ////NSLog(@"theConnection is NULL");
-    }
-    
-    
-}
-
+//-(void)Editfollowup{
+//    recordResults = FALSE;
+//    NSString *soapMessage;
+//  //  NSInteger followupID=0;
+//    soapMessage = [NSString stringWithFormat:
+//                   
+//                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+//                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+//                   
+//                   
+//                   "<soap:Body>\n"
+//                   
+//                   "<SaveFollowup xmlns=\"http://webserv.kontract360.com/\">\n"
+//                   "<HeadLine>%@</HeadLine>\n"
+//                   "<LeadLink>%d</LeadLink>\n"
+//                   "<OrgContact>%@</OrgContact>\n"
+//                   "<CommunicationType>%@</CommunicationType>\n"
+//                   "<DateContact>%@</DateContact>\n"
+//                   "<TimeContact>%@</TimeContact>\n"
+//                   "<Summary>%@</Summary>\n"
+//                   "<activityId>%d</activityId>\n"
+//                   "<followupid>%d</followupid>\n"
+//                   "</SaveFollowup>\n"
+//                   "</soap:Body>\n"
+//                   "</soap:Envelope>\n",,_ActivityId,followupID];
+//    NSLog(@"soapmsg%@",soapMessage);
+//    
+//    
+//    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+//    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
+//    
+//    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+//    
+//    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+//    
+//    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+//    
+//    [theRequest addValue: @"http://webserv.kontract360.com/SaveFollowup" forHTTPHeaderField:@"Soapaction"];
+//    
+//    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+//    [theRequest setHTTPMethod:@"POST"];
+//    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+//    
+//    
+//    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+//    
+//    if( theConnection )
+//    {
+//        _webData = [NSMutableData data];
+//    }
+//    else
+//    {
+//        ////NSLog(@"theConnection is NULL");
+//    }
+//    
+//    
+//}
+//
 
 #pragma mark - Connection
 -(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
