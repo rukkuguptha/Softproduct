b024db054398d67432a3580d538156869155320d
Riagms <riyariya44@gmail.com>
2013-06-26 13:23:14 +0530

    sf

---
+++ Newproject/commentmdl.m
@@ -0,0 +1,13 @@
+//
+//  commentmdl.m
+//  Newproject
+//
+//  Created by GMSIndia1 on 6/26/13.
+//  Copyright (c) 2013 GMSIndia1. All rights reserved.
+//
+
+#import "commentmdl.h"
+
+@implementation commentmdl
+
+@end
