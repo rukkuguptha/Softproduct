e2e2c95f4df85f8d6878b96cc082672ab8d8e810
GMSIndia 2 <rukminikr@gmsindia-2.mshome.net>
2013-06-26 12:06:43 +0530

    c

--- Newproject/ActivityViewController.m
+++ Newproject/ActivityViewController.m
@@ -199,6 +199,23 @@
 
             }
 }
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (editingStyle==UITableViewCellEditingStyleDelete) {
+       // _selectedstring=indexPath.row;
+        
+        [self deleteActivity];
+        
+        [_activityArray removeObject:indexPath];
+        
+        [self deleteActivity];
+        
+        
+        
+    }
+    
+}
+
 
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
@@ -624,6 +641,56 @@
     
     
  
+}
+-(void)deleteActivity
+{
+    
+    recordResults = FALSE;
+    NSString *soapMessage;
+     activityInfo*info2=(activityInfo*)[_activityArray objectAtIndex:_Path.row];
+    NSLog(@"%d",info2.activityId);
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   "<soap:Body>\n"
+                   "<DeleteActivity xmlns=\"http://webserv.kontract360.com/\">\n"
+                   "< <activityid>%d</activityid>\n"
+                   "</DeleteActivity>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n",info2.activityId];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://webserv.kontract360.com/DeleteActivity" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
+    
+
 }
 -(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
