0251078d20c04daa3305ffafd86a0fb198422f29
Riagms <riyariya44@gmail.com>
2013-07-02 15:12:56 +0530

    cmtd

--- CompanyViewController.h
+++ CompanyViewController.h
@@ -8,7 +8,37 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CompanyViewController : UIViewController
+@interface CompanyViewController : UIViewController{
+      BOOL recordResults;
+}
 @property(strong,nonatomic)IBOutlet UIScrollView *scroll;
+/* xmlparser*/
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
+
+
+
+
+/*outlets*/
+@property (strong, nonatomic) IBOutlet UITextField *companynametxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *statetextfld;
+
+@property (strong, nonatomic) IBOutlet UITextField *ziptextfld;
+@property (strong, nonatomic) IBOutlet UITextField *countrytxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *faxtxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *fedraltxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *stateempIdtxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *addresstxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *citytxtfld;
+
+@property (strong, nonatomic) IBOutlet UITextField *phonetxtfld;
+
+@property (strong, nonatomic) IBOutlet UITextField *mailtxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *webtxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *stateunempidtxtfld;
+
+- (IBAction)savebtn:(id)sender;
+
 
 @end
--- CompanyViewController.m
+++ CompanyViewController.m
@@ -28,13 +28,177 @@
     [super viewDidLoad];
     _scroll.frame=CGRectMake(0, 0,1024, 768);
     [_scroll setContentSize:CGSizeMake(1024,850)];
+    
+
     // Do any additional setup after loading the view from its nib.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self Getbasicinfo];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - WebService
+
+
+-(void)Getbasicinfo{
+    recordResults = FALSE;
+    NSString *soapMessage;
+    
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<GetBasicInfo xmlns=\"http://webserv.kontract360.com/\">\n"
+                   
+                   "</GetBasicInfo>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n"];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://webserv.kontract360.com/GetBasicInfo" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+
+}
+
+-(void)SaveBasicInfo{
+    recordResults = FALSE;
+    NSString *soapMessage;
+    
+   NSString * letter=@"";
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<SaveBasicInfo xmlns=\"http://webserv.kontract360.com/\">\n"
+                   "<CompanyName>%@</CompanyName>\n"
+                   "<Address>%@</Address>\n"
+                   "<City>%@</City>\n"
+                   "<State>%@</State>\n"
+                   "<Zip>%@</Zip>\n"
+                   "<Country>%@</Country>\n"
+                   "<Phone>%@</Phone>\n"
+                   "<Fax>%@</Fax>\n"
+                   "<email>%@</email>\n"
+                   "<WebSite>%@</WebSite>\n"
+                   "<FederalEmployerId>%@</FederalEmployerId>\n"
+                   "<StateEmployerId>%@</StateEmployerId>\n"
+                   "<StateUnemploymentId>%@</StateUnemploymentId>\n"
+                   "<LetterHead>%@</LetterHead>\n"
+                   "</SaveBasicInfo>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n",_companynametxtfld.text,_addresstxtfld.text,_citytxtfld.text,_statetextfld.text,_ziptextfld.text,_countrytxtfld.text,_phonetxtfld.text,_faxtxtfld.text,_mailtxtfld.text,_webtxtfld.text,_fedraltxtfld.text,_stateempIdtxtfld.text,_stateunempidtxtfld.text,letter];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://webserv.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://webserv.kontract360.com/SaveBasicInfo" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
+}
+
+#pragma mark - Connection
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+	[_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:@"Alert" message:@"ERROR with theConenction" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+	
+	
+	if( _xmlParser )
+	{
+		
+	}
+	
+	_xmlParser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlParser setDelegate:(id)self];
+	[_xmlParser setShouldResolveExternalEntities: YES];
+	[_xmlParser parse];
+      
+    
+}
+
+
+
+
+
+#pragma mark - Buttons
+- (IBAction)savebtn:(id)sender {
+    [self SaveBasicInfo];
+}
 @end
