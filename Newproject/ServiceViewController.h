472d966aebc71a8a94b046f559834349880e6087
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2013-12-31 11:10:54 +0530

    committed

--- Newproject/ServiceViewController.h
+++ Newproject/ServiceViewController.h
@@ -12,15 +12,39 @@
 
 {
     NSInteger optionidentifier;
+    BOOL recordResults;
+    NSInteger path;
 }
 
--(IBAction)closetheservicesview:(id)sender;
+/* xmlparser*/
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
+
+
+/*IBOutlets*/
 @property(strong,nonatomic)IBOutlet UITableView *servicesTable;
 @property(strong,nonatomic)IBOutlet UIView *titleview;
--(IBAction)Addservices:(id)sender;
 @property(strong,nonatomic)IBOutlet UITableViewCell *servicecell;
--(IBAction)editservices:(id)sender;
 @property(strong,nonatomic)IBOutlet UIView *addserview;
--(IBAction)closeaddview:(id)sender;
 @property(strong,nonatomic)IBOutlet UINavigationItem *navabar;
+/*cell outlets*/
+
+@property (strong, nonatomic) IBOutlet UILabel *servicelabel;
+@property (strong, nonatomic) IBOutlet UIButton *editbtn;
+
+/*IBActions*/
+-(IBAction)closetheservicesview:(id)sender;
+-(IBAction)Addservices:(id)sender;
+-(IBAction)editservices:(id)sender;
+-(IBAction)closeaddview:(id)sender;
+-(IBAction)deleteservices:(id)sender;
+
+/*actions in addview*/
+-(IBAction)updateservice:(id)sender;
+-(IBAction)cancelservice:(id)sender;
+
+/*outlets in addview*/
+@property(strong,nonatomic)IBOutlet UITextField *servicetextfld;
+
 @end
--- Newproject/jobsitereqViewController.m
+++ Newproject/jobsitereqViewController.m
@@ -26,6 +26,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _jobsitetable.layer.borderWidth = 2.0;
+    _jobsitetable.layer.borderColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    _titleview.backgroundColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    _searchbar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, 220, 44)];
+    _searchbar.delegate = (id)self;
+    _searchbar.tintColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    
+    self.jobsitetable.tableHeaderView =_searchbar;
+    
+    UISearchDisplayController* searchController = [[UISearchDisplayController alloc] initWithSearchBar:_searchbar contentsController:self];
+    searchController.searchResultsDataSource = (id)self;
+    searchController.searchResultsDelegate =(id)self;
+    searchController.delegate = (id)self;
+
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -34,6 +48,65 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"mycell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        [[NSBundle mainBundle]loadNibNamed:@"customjobcell" owner:self options:nil];
+        cell=_jobcell;
+    }
+    
+    return cell;
+}
+
+#pragma mark - Table View delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    
+}
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    //alternating cell back ground color
+    if(tableView==_jobsitetable)
+    {
+        if (indexPath.row%2 == 0) {
+            [cell setBackgroundColor:[UIColor whiteColor]];
+            
+        }else
+        {
+            
+            //[cell setBackgroundColor:[UIColor colorWithRed:247.0/255.0f green:247.0/255.0f blue:247.0/255.0f alpha:1.0f]];
+            [cell setBackgroundColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f]];
+            
+            
+        }
+    }
+}
+-(IBAction)addjobview:(id)sender
+{
+    _addjobview.hidden=NO;
+}
+-(IBAction)closeaddjbview:(id)sender
+{
+    _addjobview.hidden=YES;
+}
+
 -(IBAction)closejobreqview:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
