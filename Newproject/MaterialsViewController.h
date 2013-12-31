f43742e2f4e6fe0bdc29a642e935e480de634899
Riagms <riyariya44@gmail.com>
2013-12-31 16:02:28 +0530

    cmtd

--- Newproject/MaterialsViewController.h
+++ Newproject/MaterialsViewController.h
@@ -7,10 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Manpwr.h"
 
-@interface MaterialsViewController : UIViewController
+@interface MaterialsViewController : UIViewController{
+     BOOL recordResults;
+    
+}
+
+@property(strong,nonatomic)Manpwr *materialmdl;
+
+@property(strong,nonatomic)NSMutableArray *materialarray;
+/*cell outlets*/
+
+@property (strong, nonatomic) IBOutlet UILabel *codelbl;
+@property (strong, nonatomic) IBOutlet UILabel *deslbl;
+@property (strong, nonatomic) IBOutlet UILabel *typelbl;
+@property (strong, nonatomic) IBOutlet UILabel *costlbl;
 
 
+/* xmlparser*/
+@property(strong,nonatomic)NSXMLParser *xmlParser;
+@property(strong,nonatomic)NSMutableString *soapResults;
+@property(strong,nonatomic)NSMutableData *webData;
 //outlets
 @property(strong,nonatomic)IBOutlet UITableView *materialTable;
 @property(strong,nonatomic)IBOutlet UITableViewCell *materialCell;
@@ -18,7 +36,7 @@
 @property(strong,nonatomic)IBOutlet UINavigationItem *navItem;
 @property(strong,nonatomic)IBOutlet UIView *addmatView;
 
-@property(strong,nonatomic)UISearchBar *searchbar;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchbar;
 
 -(IBAction)closeMaterials:(id)sender;
 -(IBAction)addmaterial:(id)sender;
--- Newproject/MaterialsViewController.m
+++ Newproject/MaterialsViewController.m
@@ -26,6 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     _materialTable.layer.borderWidth = 2.0;
     _materialTable.layer.borderColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
     _titleView.backgroundColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
@@ -44,6 +45,10 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self SelectAllMaterials];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -58,7 +63,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 5;
+    return [_materialarray count];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -72,6 +77,16 @@
         [[NSBundle mainBundle]loadNibNamed:@"custommaterialcell" owner:self options:nil];
         cell=_materialCell;
     }
+    Manpwr*materaialmdl=(Manpwr *)[_materialarray objectAtIndex:indexPath.row];
+    _codelbl=(UILabel *)[cell viewWithTag:1];
+    _codelbl.text=materaialmdl.itemcode;
+    _deslbl=(UILabel *)[cell viewWithTag:2];
+    _deslbl.text=materaialmdl.itemdescptn;
+    _typelbl=(UILabel *)[cell viewWithTag:3];
+    _typelbl.text=materaialmdl.subtype;
+    _costlbl=(UILabel *)[cell viewWithTag:4];
+    _costlbl.text=materaialmdl.unitcost;
+
     
     return cell;
 }
@@ -83,6 +98,215 @@
     
     
 }
+
+
+
+#pragma mark- WebService
+-(void)SelectAllMaterials{
+    recordResults = FALSE;
+    NSString *soapMessage;
+    
+    
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<SelectAllMaterials xmlns=\"http://ios.kontract360.com/\">\n"
+                   
+                   "</SelectAllMaterials>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n"];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://ios.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://ios.kontract360.com/SelectAllMaterials" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
+}
+#pragma mark - Connection
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+	[_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:@"Alert" message:@"ERROR with theConenction" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+	
+	
+	if( _xmlParser )
+	{
+		
+	}
+	
+	_xmlParser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlParser setDelegate:(id)self];
+	[_xmlParser setShouldResolveExternalEntities: YES];
+	[_xmlParser parse];
+    [_materialTable reloadData];
+  
+    
+    
+}
+#pragma mark-xml parser
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *) namespaceURI qualifiedName:(NSString *)qName
+   attributes: (NSDictionary *)attributeDict{
+    if([elementName isEqualToString:@"SelectAllMaterialsResult"])
+    {
+        _materialarray=[[NSMutableArray alloc]init];
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"EntryId"])
+    {
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ItemCode"])
+    {
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"Description"])
+    {
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"SubType"])
+    {
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"UnitCost"])
+    {
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    
+}
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    
+    
+    
+	if( recordResults )
+        
+	{
+        
+        
+		[_soapResults appendString: string];
+    }
+}
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+    if([elementName isEqualToString:@"EntryId"])
+    {
+        _materialmdl=[[Manpwr alloc]init];
+        
+        recordResults = FALSE;
+        
+        _materialmdl.entryid=[_soapResults integerValue];
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"ItemCode"])
+    {
+        
+        recordResults = FALSE;
+      _materialmdl.itemcode=_soapResults;
+        
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"Description"])
+    {
+        
+        recordResults = FALSE;
+        
+       _materialmdl.itemdescptn=_soapResults;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"SubType"])
+    {
+        
+        recordResults = FALSE;
+       _materialmdl.subtype=_soapResults;
+        
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"UnitCost"])
+    {
+        
+        recordResults = FALSE;
+        
+         _materialmdl.unitcost=_soapResults;
+        [_materialarray addObject:_materialmdl];
+        _soapResults = nil;
+    }
+    
+}
+
 
 //IBActions
 
