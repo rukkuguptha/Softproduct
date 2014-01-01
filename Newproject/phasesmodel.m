562266c8444e158dd3a49427b35cf7eb5d5efdfa
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-01-01 11:54:10 +0530

    cc

---
+++ Newproject/phasesmodel.m
@@ -0,0 +1,13 @@
+//
+//  phasesmodel.m
+//  Newproject
+//
+//  Created by GMSIndia 2 on 01/01/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import "phasesmodel.h"
+
+@implementation phasesmodel
+
+@end
