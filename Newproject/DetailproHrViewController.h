82b2c49a56522d6e97e71b9df07f0753af724591
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-02-11 13:55:17 +0530

    cmted

--- Newproject/DetailproHrViewController.h
+++ Newproject/DetailproHrViewController.h
@@ -9,27 +9,59 @@
 #import <UIKit/UIKit.h>
 
 @interface DetailproHrViewController : UIViewController
+{
+    NSInteger poptype;
+}
 
 - (IBAction)DetailsBtnAction:(id)sender;
 - (IBAction)DcmntBtnAction:(id)sender;
 - (IBAction)w4BtnAction:(id)sender;
 - (IBAction)paymentbtnaction:(id)sender;
 
-//Detailsview
+/*Detailsview*/
 @property(strong,nonatomic)IBOutlet UIView *detailsaddview;
 @property(strong,nonatomic)IBOutlet UITableView *detailstablview;
 @property(strong,nonatomic)IBOutlet UIView *detailstabletitleview;
 @property(strong,nonatomic)IBOutlet UITableViewCell *detailscell;
 
-//W4 view
-@property(strong,nonatomic)IBOutlet UIView *w4addview;
+//celldetail labels
+@property(strong,nonatomic)IBOutlet UILabel *requirementlabel;
+@property(strong,nonatomic)IBOutlet UILabel *statuslabel;
+@property(strong,nonatomic)IBOutlet UILabel *expirylabel;
+@property(strong,nonatomic)IBOutlet UILabel *verificationlabel;
+@property(strong,nonatomic)IBOutlet UIButton *editbtn;
+@property(strong,nonatomic)IBOutlet UIView *editview;
+-(IBAction)editdetails:(id)sender;
+-(IBAction)closeeditview:(id)sender;
+-(IBAction)updaterequirement:(id)sender;
+-(IBAction)cancelrequirement:(id)sender;
+-(IBAction)selectstatus:(id)sender;
+-(IBAction)verifiedaction:(id)sender;
+
+/*W4 view*/
+@property(strong,nonatomic)IBOutlet UIView *w4detailview;
 @property(strong,nonatomic)IBOutlet UIButton *maritalbtn;
+@property(strong,nonatomic)IBOutlet UITextField *Dependentstexffld;
 
 
 -(IBAction)selectmaritalstatus:(id)sender;
 
+/*Paymentypeview*/
+@property(strong,nonatomic)IBOutlet UIView *paymentdetailview;
+@property(strong,nonatomic)IBOutlet UIButton *paymenttypebtn;
 
 
+-(IBAction)selectpaymenttype:(id)sender;
+
+/*document View*/
+@property(strong,nonatomic)IBOutlet UIView *dcmntdetailview;
+@property(strong,nonatomic)IBOutlet UIView *documentnametextfld;
+@property(strong,nonatomic)IBOutlet UITableView *documentlisttable;
+@property(strong,nonatomic)IBOutlet UITableViewCell *doccell;
+@property(strong,nonatomic)IBOutlet UIView *doctabletitleview;
+
+-(IBAction)selectfileaction:(id)sender;
+-(IBAction)updatedoc:(id)sender;
 
 /*popover*/
 @property(strong,nonatomic)UIPopoverController *popOverController;
--- Newproject/DetailproHrViewController.m
+++ Newproject/DetailproHrViewController.m
@@ -26,7 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _detailsaddview.hidden=NO;;
     _detailstabletitleview.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+   _documentlisttable.layer.borderColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    _doctabletitleview.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+
     _maritalarray=[[NSMutableArray alloc]initWithObjects:@"SINGLE",@"MARRIED",@"DIVORCED", nil];
     // Do any additional setup after loading the view from its nib.
 }
@@ -40,18 +44,45 @@
 - (IBAction)DetailsBtnAction:(id)sender
 {
     _detailsaddview.hidden=NO;
+    _w4detailview.hidden=YES;
+    _paymentdetailview.hidden=YES;
+    _dcmntdetailview.hidden=YES;
 }
 
-- (IBAction)DcmntBtnAction:(id)sender {
+- (IBAction)DcmntBtnAction:(id)sender
+{
+    _dcmntdetailview.hidden=NO;
+    _w4detailview.hidden=YES;
+    _detailsaddview.hidden=YES;
+    _paymentdetailview.hidden=YES;
 }
 
 - (IBAction)w4BtnAction:(id)sender
 {
-    _w4addview.hidden=NO;
+    _w4detailview.hidden=NO;
+     _detailsaddview.hidden=YES;
+    _paymentdetailview.hidden=YES;
+    _dcmntdetailview.hidden=YES;
+
 }
 
-- (IBAction)paymentbtnaction:(id)sender {
+- (IBAction)paymentbtnaction:(id)sender
+{
+    _paymentdetailview.hidden=NO;
+    _w4detailview.hidden=YES;
+    _detailsaddview.hidden=YES;
+    _dcmntdetailview.hidden=YES;
+
 }
+-(IBAction)editdetails:(id)sender
+{
+    _editview.hidden=NO;
+}
+-(IBAction)closeeditview:(id)sender
+{
+    _editview.hidden=YES;
+}
+
 
 #pragma mark-tableview datasource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -66,6 +97,10 @@
     if(tableView==_detailstablview)
     {
          return 5;
+    }
+    if(tableView==_documentlisttable)
+    {
+        return 5;
     }
     if (tableView==_popOverTableView) {
         return [_maritalarray count];
@@ -82,15 +117,23 @@
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-        [[NSBundle mainBundle]loadNibNamed:@"proHRDetailcell" owner:self options:nil];
-        cell=_detailscell;
-        //        cell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
-    }
-    if(tableView==_popOverTableView)
-    {
-        
-    }
+        if(tableView==_detailstablview)
+        {
+            [[NSBundle mainBundle]loadNibNamed:@"proHRDetailcell" owner:self options:nil];
+            cell=_detailscell;
+        }
+        if(tableView==_documentlisttable)
+        {
+            [[NSBundle mainBundle]loadNibNamed:@"customdoccell" owner:self options:nil];
+            cell=_doccell;
+        }
     
+    if (tableView==_popOverTableView) {
+        cell.textLabel.font = [UIFont fontWithName:@"Helvetica Neue Light" size:12];
+        cell.textLabel.font = [UIFont systemFontOfSize:12.0];
+        cell.textLabel.text=[_maritalarray objectAtIndex:indexPath.row];
+    }
+    }
     
     return cell;
 }
@@ -125,7 +168,7 @@
 
 -(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
     //alternating cell back ground color
-    if(tableView==_detailstablview)
+    if(tableView==_detailstablview||tableView==_documentlisttable)
     {
         if (indexPath.row%2 == 0) {
             [cell setBackgroundColor:[UIColor whiteColor]];
@@ -143,6 +186,7 @@
 /*popover*/
 -(IBAction)selectmaritalstatus:(id)sender
 {
+    poptype=1;
     UIViewController* popoverContent = [[UIViewController alloc]
                                         init];
     
@@ -175,10 +219,50 @@
     
     
     [self.popOverController presentPopoverFromRect:_maritalbtn.frame
-                                            inView:self.w4addview
+                                            inView:self.w4detailview
                           permittedArrowDirections:UIPopoverArrowDirectionUp
                                           animated:YES];
     
+
+}
+-(IBAction)selectpaymenttype:(id)sender
+{
+    poptype=2;
+    UIViewController* popoverContent = [[UIViewController alloc]
+                                        init];
+    
+    UIView* popoverView = [[UIView alloc]
+                           initWithFrame:CGRectMake(0, 0, 160, 90)];
+    
+    popoverView.backgroundColor = [UIColor whiteColor];
+    _popOverTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, 160, 90)];
+    
+    _popOverTableView.delegate=(id)self;
+    _popOverTableView.dataSource=(id)self;
+    _popOverTableView.rowHeight= 32;
+    _popOverTableView.separatorStyle=UITableViewCellSeparatorStyleSingleLine;
+    
+    
+    [popoverView addSubview:_popOverTableView];
+    popoverContent.view = popoverView;
+    
+    //resize the popover view shown
+    //in the current view to the view's size
+    popoverContent.contentSizeForViewInPopover = CGSizeMake(160, 90);
+    
+    //create a popover controller
+    
+    self.popOverController = [[UIPopoverController alloc]
+                              initWithContentViewController:popoverContent];
+    
+    
+    
+    
+    
+    [self.popOverController presentPopoverFromRect:_paymenttypebtn.frame
+                                            inView:self.paymentdetailview
+                          permittedArrowDirections:UIPopoverArrowDirectionUp
+                                          animated:YES];
 
 }
 
