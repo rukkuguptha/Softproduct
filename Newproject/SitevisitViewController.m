0808e27c14310b56edbf36dd670228cc262718a9
Riagms <riyariya44@gmail.com>
2014-02-14 14:26:44 +0530

    cmmtd

--- Newproject/SitevisitViewController.m
+++ Newproject/SitevisitViewController.m
@@ -29,6 +29,10 @@
     // Do any additional setup after loading the view from its nib.
     
     [[UITabBar appearance] setTintColor:[UIColor redColor]];
+    _prdunratetble.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    _prdunratetble.layer.borderWidth=3.0;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,9 +40,52 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark-tableview datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+    
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"mycell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        [[NSBundle mainBundle]loadNibNamed:@"Ratecellview" owner:self options:nil];
+    }
+   // cell.textLabel.text=[_serviceaddedarray objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
 -(IBAction)closesitevisit:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
+- (IBAction)cmpxtyofwrkbtn:(id)sender {
+}
+
+- (IBAction)updatebtn:(id)sender {
+}
+- (IBAction)ratedeletebtn:(id)sender {
+}
+
+- (IBAction)Rateaddbtn:(id)sender {
+}
+
+- (IBAction)celleditbtn:(id)sender {
+}
 @end
