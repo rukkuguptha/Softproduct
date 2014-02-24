885cd37e90daebaadcdbca87526f75dfef62de8e
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-02-24 12:24:45 +0530

    cmted

--- Newproject/DetailplanViewController.h
+++ Newproject/DetailplanViewController.h
@@ -9,6 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface DetailplanViewController : UIViewController
+{
+    BOOL recordResults;
+}
+
+@property(strong,nonatomic)NSString *planid;
+
 
 /*IBActions*/
 -(IBAction)closeplan:(id)sender;
@@ -28,7 +34,27 @@
 @property(strong,nonatomic)IBOutlet UITableViewCell *scaffoldcell;
 @property(strong,nonatomic)IBOutlet UITableView *scaffoldtable;
 
+/*outlets in cell*/
+@property(strong,nonatomic)IBOutlet UILabel *unitcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *equipcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *pjctheadercelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *scaffoldcellalbel;
+@property(strong,nonatomic)IBOutlet UILabel *lengthcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *heightcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *widthcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *quantitycelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *elevationcelllabel;
+@property(strong,nonatomic)IBOutlet UILabel *manhourslabel;
+
+
+/*xmlParser*/
+@property(strong,nonatomic)NSMutableData *webData;
+@property(strong,nonatomic)NSMutableString *soapresults;
+@property(strong,nonatomic)NSXMLParser *xmlparser;
 
 -(IBAction)addplan:(id)sender;
 
+
+/*arrays and Dicts*/
+@property(strong,nonatomic)NSMutableArray *scaffoldingplanlistarray;
 @end
--- Newproject/DetailplanViewController.m
+++ Newproject/DetailplanViewController.m
@@ -37,6 +37,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self ScaffoldingSelectplan];
+}
 -(IBAction)closeplan:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
@@ -103,7 +108,216 @@
     return cell;
     
 }
+-(void)ScaffoldingSelectplan{
+    recordResults = FALSE;
+    NSString *soapMessage;
+    
+    
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<ScaffoldingSelectplan xmlns=\"http://ios.kontract360.com/\">\n"
+                    "<planid>%@</planid>\n"
+                   "</ScaffoldingSelectplan>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n",_planid];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://ios.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://ios.kontract360.com/ScaffoldingSelectplan" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
+}
+#pragma mark - Connection
+-(void)connection:(NSURLConnection*)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:@"Alert" message:@"ERROR with the Connection" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+	
+	
+	if( _xmlparser )
+	{
+		
+	}
+	
+	_xmlparser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlparser setDelegate:(id)self];
+	[_xmlparser setShouldResolveExternalEntities: YES];
+	[_xmlparser parse];
 
 
+}
+#pragma mark-xml parser
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *) namespaceURI qualifiedName:(NSString *)qName
+   attributes: (NSDictionary *)attributeDict{
+    if([elementName isEqualToString:@"ScaffoldingSelectplanResult"])
+    {
+        _scaffoldingplanlistarray=[[NSMutableArray alloc]init];
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"id"])
+    {
+       
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"length"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"width"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"height"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"qty"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"elevation"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"unit"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"equp"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ph"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"typeofscaffold"])
+    {
+        
+        if(!_soapresults)
+        {
+            _soapresults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+
+
+
+
+}
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    
+    
+    
+	if( recordResults )
+        
+	{
+        [_soapresults appendString: string];
+    }
+}
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+    if([elementName isEqualToString:@"ScaffoldingSelectplanResult"])
+    {
+        
+        recordResults = FALSE;
+        
+        _soapresults = nil;
+    }
+}
 
 @end
