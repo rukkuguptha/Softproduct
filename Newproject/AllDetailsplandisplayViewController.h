7719ba56d2c59aaa6a42f6e247f18a3da3d6b851
Riagms <riyariya44@gmail.com>
2014-02-25 12:10:44 +0530

    cmmtd

--- Newproject/AllDetailsplandisplayViewController.h
+++ Newproject/AllDetailsplandisplayViewController.h
@@ -11,8 +11,13 @@
 #import "Customscaffoldingplan.h"
 #import <QuartzCore/QuartzCore.h>
 
-@interface AllDetailsplandisplayViewController : UIViewController{
+@interface AllDetailsplandisplayViewController : UIViewController<UIGestureRecognizerDelegate>{
     
+    BOOL            dragFromSource;     // used for reodering
+    NSIndexPath*    pathFromDstTable;
+    UITableViewCell*    draggedCell;
+    IBOutlet UIView *droparea;
+     id                  draggedData;
 }
 
 @property(strong,nonatomic)Scaffoldtypemdl *sccfldtypemdl;
@@ -28,6 +33,11 @@
 
 @property (strong, nonatomic) IBOutlet UITableViewCell *subtypecell;
 @property (strong, nonatomic) IBOutlet UITableViewCell *maintblecell;
+@property (strong, nonatomic) IBOutlet UIView *tuchgview;
+
+
+
+/*for drag and Drop*/
 
 
 @end
