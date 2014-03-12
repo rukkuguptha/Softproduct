0634924727de78702acf340e9dfcf182d2992aaa
Riagms <riyariya44@gmail.com>
2014-03-12 12:49:47 +0530

    cmmtd

--- Newproject/TilecustmrViewController.h
+++ Newproject/TilecustmrViewController.h
@@ -8,12 +8,18 @@
 
 #import <UIKit/UIKit.h>
 #import "NewCustmrViewController.h"
+#import "ContractViewController.h"
+#import "MarkupViewController.h"
+#import "BillingViewController.h"
 
 @interface TilecustmrViewController : UIViewController
 
 - (IBAction)clsebtn:(id)sender;
 @property (strong, nonatomic) IBOutlet UINavigationBar *navgtnbar;
 @property (strong, nonatomic) NewCustmrViewController *custmrVCtrl;
+@property (strong, nonatomic)ContractViewController *cntrctVCtrl;
+@property (strong, nonatomic)MarkupViewController *markVCtrl;
+@property (strong, nonatomic)BillingViewController *billgVCtrl;
 @property (strong, nonatomic) IBOutlet UIView *custmrview;
 @property (strong, nonatomic) IBOutlet UIView *cntrctview;
 @property (strong, nonatomic) IBOutlet UIView *markupview;
--- Newproject/TilecustmrViewController.m
+++ Newproject/TilecustmrViewController.m
@@ -38,6 +38,27 @@
     doubleTap.numberOfTapsRequired=1;
     doubleTap.delegate=(id)self;
     [self.custmrview addGestureRecognizer:doubleTap];
+    UITapGestureRecognizer *doubleTap2 = [[UITapGestureRecognizer alloc]
+                                         initWithTarget:self
+                                         action:@selector(contractpage)];
+    doubleTap2.numberOfTapsRequired=1;
+    doubleTap2.delegate=(id)self;
+    [self.cntrctview addGestureRecognizer:doubleTap2];
+
+    UITapGestureRecognizer *doubleTap3 = [[UITapGestureRecognizer alloc]
+                                         initWithTarget:self
+                                         action:@selector(markuppage)];
+    doubleTap3.numberOfTapsRequired=1;
+    doubleTap3.delegate=(id)self;
+    [self.markupview addGestureRecognizer:doubleTap3];
+
+    UITapGestureRecognizer *doubleTap4 = [[UITapGestureRecognizer alloc]
+                                         initWithTarget:self
+                                         action:@selector(billingpage)];
+    doubleTap4.numberOfTapsRequired=1;
+    doubleTap4.delegate=(id)self;
+    [self.billingview addGestureRecognizer:doubleTap4];
+
 
 }
 - (void)didReceiveMemoryWarning
@@ -58,6 +79,38 @@
 
     
 }
+-(void)contractpage{
+    if (!self.cntrctVCtrl) {
+        self.cntrctVCtrl=[[ContractViewController alloc]initWithNibName:@"ContractViewController" bundle:nil];
+    }
+    //_custmrVCtrl.modalPresentationStyle = UIModalPresentationPageSheet;
+    
+    [self presentViewController:_cntrctVCtrl
+                       animated:YES completion:NULL];
+
+}
+-(void)markuppage{
+    if (!self.markVCtrl) {
+        self.markVCtrl=[[MarkupViewController alloc]initWithNibName:@"MarkupViewController" bundle:nil];
+    }
+    //_custmrVCtrl.modalPresentationStyle = UIModalPresentationPageSheet;
+    
+    [self presentViewController:_markVCtrl
+                       animated:YES completion:NULL];
+    
+}
+-(void)billingpage{
+    if (!self.billgVCtrl) {
+        self.billgVCtrl=[[BillingViewController alloc]initWithNibName:@"BillingViewController" bundle:nil];
+    }
+    //_custmrVCtrl.modalPresentationStyle = UIModalPresentationPageSheet;
+    
+    [self presentViewController:_billgVCtrl
+                       animated:YES completion:NULL];
+    
+}
+
+
 - (IBAction)clsebtn:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
