76b41eafea8f2c99c25f11b00310e1a3e22838c2
Riagms <riyariya44@gmail.com>
2014-03-25 12:54:25 +0530

    cvdfvdf

--- Newproject/ContactInfoViewController.m
+++ Newproject/ContactInfoViewController.m
@@ -40,38 +40,225 @@
 }
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
+    [self CustomercontactInfoselect];
+    
+    }
+#pragma mark-webservice
+-(void)CustomercontactInfoselect{
+    recordResults = FALSE;
+    NSString *soapMessage;
     
     
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<CustomercontactInfoselect xmlns=\"http://ios.kontract360.com/\">\n"
+                   
+                   "</CustomercontactInfoselect>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n"];
+    NSLog(@"soapmsg%@",soapMessage);
     
-    UIBarButtonItem*addbtn=[[UIBarButtonItem alloc]initWithTitle:@"Add" style:UIBarButtonItemStylePlain target:self action:@selector(addaction)];
-    UIBarButtonItem*editbtn=[[UIBarButtonItem alloc]initWithTitle:@"Delete" style:UIBarButtonItemStylePlain target:self action:@selector(editaction)];
-    NSArray*barbutns=[[NSArray alloc]initWithObjects:addbtn,editbtn, nil];
-    [self.navigationItem setRightBarButtonItems:barbutns animated:YES];
-    _addview.hidden=YES;
+    
+    // NSURL *url = [NSURL URLWithString:@"http://192.168.0.146/link/service.asmx"];
+    NSURL *url = [NSURL URLWithString:@"http://ios.kontract360.com/service.asmx"];
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://ios.kontract360.com/CustomercontactInfoselect" forHTTPHeaderField:@"Soapaction"];
+    
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
     
     
 }
--(void)editaction
+#pragma mark - Connection
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
-    if ([self.contactinfoTable isEditing]) {
-        // If the tableView is already in edit mode, turn it off. Also change the title of the button to reflect the intended verb (‘Edit’, in this case).
-        
-        [self.contactinfoTable setEditing:NO animated:YES];
-        //[_Editbtn setTitle:@"Edit"forState:UIControlStateNormal];
-    }
-    else {
-        // [_Editbtn setTitle:@"Done"forState:UIControlStateNormal];
-        
-        // Turn on edit mode
-        
-        [self.contactinfoTable setEditing:YES animated:YES];
-    }
+	[_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:@"Alert" message:@"ERROR with theConnection" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+	
+	
+	if( _xmlParser )
+	{
+		
+	}
+	
+	_xmlParser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlParser setDelegate:(id)self];
+	[_xmlParser setShouldResolveExternalEntities: YES];
+	[_xmlParser parse];
+    
+    [_contactinfoTable reloadData];
     
 }
+#pragma mark - XMLParser
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *) namespaceURI qualifiedName:(NSString *)qName
+   attributes: (NSDictionary *)attributeDict{
+    
+    if([elementName isEqualToString:@"CustomercontactInfoselectResult"])
+    {
+        //_customerarray=[[NSMutableArray alloc]init];
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"EntryId"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"CustomerCode"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ContactName"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ContactMobile"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ContactPhone"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    if([elementName isEqualToString:@"ContactFax"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    if([elementName isEqualToString:@"PositionCode"])
+    {
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    }
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    
+    
+    
+	if( recordResults )
+        
+	{
+        
+        
+		[_soapResults appendString: string];
+    }
+}
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+    if([elementName isEqualToString:@"EntryId"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"CustomerCode"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"ContactName"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
 
--(void)addaction
-{
-    _addview.hidden=NO;
+    }
+    if([elementName isEqualToString:@"ContactMobile"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+
+    }
+    if([elementName isEqualToString:@"ContactPhone"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+
+    }
+    
+    if([elementName isEqualToString:@"ContactFax"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+    }
+    
+    if([elementName isEqualToString:@"PositionCode"])
+    {
+        recordResults = FALSE;
+        _soapResults = nil;
+    }
+    
+  
 }
 - (IBAction)editcellbtn:(id)sender
 {
@@ -96,7 +283,7 @@
 - (IBAction)editbtn:(id)sender {
 }
 
-
+#pragma mark-Tableview Delegate
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     
@@ -125,6 +312,8 @@
     
     
 }
+
+#pragma mark-Tableview datasources
 -(void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
     
     
@@ -137,84 +326,9 @@
         
     }else
     {
-        
-        //[cell setBackgroundColor:[UIColor colorWithRed:247.0/255.0f green:247.0/255.0f blue:247.0/255.0f alpha:1.0f]];
         [cell setBackgroundColor:[UIColor colorWithRed:227.0/255.0f green:240.0/255.0f blue:247.0/255.0f alpha:1.0f]];
     }
 }
-//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    if (tableView==_customertable) {
-//        // CGRect frame = [_popOverTableView cellForRowAtIndexPath:indexPath].frame;
-//        // poptype=5;
-//        UIViewController* popoverContent = [[UIViewController alloc]
-//                                            init];
-//        
-//        UIView* popoverView = [[UIView alloc]
-//                               initWithFrame:CGRectMake(0, 0, 120, 70)];
-//        
-//        popoverView.backgroundColor = [UIColor whiteColor];
-//        _popOverTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, 120, 70)];
-//        
-//        _popOverTableView.delegate=(id)self;
-//        _popOverTableView.dataSource=(id)self;
-//        _popOverTableView.rowHeight= 32;
-//        _popOverTableView.separatorStyle=UITableViewCellSeparatorStyleSingleLineEtched;
-//        
-//        
-//        // CGRect rect = frame;
-//        [popoverView addSubview:_popOverTableView];
-//        popoverContent.view = popoverView;
-//        
-//        //resize the popover view shown
-//        //in the current view to the view's size
-//        popoverContent.contentSizeForViewInPopover = CGSizeMake(120, 70);
-//        
-//        //create a popover controller
-//        
-//        self.popOverController = [[UIPopoverController alloc]
-//                                  initWithContentViewController:popoverContent];
-//        
-//        
-//        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-//        CGRect rect=CGRectMake(cell.bounds.origin.x+90, cell.bounds.origin.y+10, 50, 30);
-//        [self.popOverController presentPopoverFromRect:rect inView:cell permittedArrowDirections:nil animated:YES];
-//        
-//        //    [self.popOverController presentPopoverFromRect:rect
-//        //                                            inView:self.leadTable
-//        //                          permittedArrowDirections:UIPopoverArrowDirectionLeft
-//        //                                          animated:YES];
-//        //
-//        //  [self MenuAction];
-//    }
-//    
-//    if (tableView==_popOverTableView) {
-//        if (indexPath.row==0) {
-//            if (!self.cntctVctrl) {
-//                self.cntctVctrl=[[ContactInfoViewController alloc]initWithNibName:@"ContactInfoViewController" bundle:nil];
-//            }
-//            [self.navigationController pushViewController:self.cntctVctrl animated:YES];
-//            
-//        }
-//        
-//        if (indexPath.row==1) {
-//            
-//            if (!self.salesVCtrl) {
-//                self.salesVCtrl=[[SalesRepInfoViewController alloc]initWithNibName:@"SalesRepInfoViewController" bundle:nil];
-//            }
-//            [self.navigationController pushViewController:self.salesVCtrl animated:YES];
-//            
-//            
-//            
-//        }
-//        
-//        [self.popOverController dismissPopoverAnimated:YES];
-//        
-//        
-//        
-//    }
-//    
-//}
 
 
 @end
