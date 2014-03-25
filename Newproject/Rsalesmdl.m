8e511d775f091ad2c70a0eb4238cbe45b4674c7d
Riagms <riyariya44@gmail.com>
2014-03-25 13:52:38 +0530

    CM

---
+++ Newproject/Rsalesmdl.m
@@ -0,0 +1,13 @@
+//
+//  Rsalesmdl.m
+//  Newproject
+//
+//  Created by GMSIndia1 on 3/25/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import "Rsalesmdl.h"
+
+@implementation Rsalesmdl
+
+@end
