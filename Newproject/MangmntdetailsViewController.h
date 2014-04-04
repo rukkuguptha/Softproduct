6c6b1375c4e296574cd44af51956afbc6a222879
Riagms <riyariya44@gmail.com>
2014-04-04 08:46:54 +0530

    cmmtd

--- Newproject/MangmntdetailsViewController.h
+++ Newproject/MangmntdetailsViewController.h
@@ -10,4 +10,130 @@
 
 @interface MangmntdetailsViewController : UIViewController
 
+
+
+
+@property(strong,nonatomic)NSMutableArray *detailsarray;
+
+/*tabbarbuttons*/
+- (IBAction)paymntbtn:(id)sender;
+- (IBAction)volumebtn:(id)sender;
+- (IBAction)laborbtn:(id)sender;
+- (IBAction)equpbtn:(id)sender;
+- (IBAction)othrtermsbtn:(id)sender;
+- (IBAction)markupbtn:(id)sender;
+- (IBAction)basebtn:(id)sender;
+- (IBAction)fuelbtn:(id)sender;
+- (IBAction)nonfuelbtn:(id)sender;
+- (IBAction)SMRbtn:(id)sender;
+- (IBAction)Orbtn:(id)sender;
+- (IBAction)deliveryratebtn:(id)sender;
+- (IBAction)srsbtn:(id)sender;
+- (IBAction)docubtn:(id)sender;
+
+
+
+
+
+/*payment*/
+
+@property (strong, nonatomic) IBOutlet UIView *paymentview;
+@property (strong, nonatomic) IBOutlet UITextView *clausetxtview;
+@property (strong, nonatomic) IBOutlet UITextField *nettextfld;
+
+/*volume*/
+@property (strong, nonatomic) IBOutlet UIView *volumeview;
+@property (strong, nonatomic) IBOutlet UITextView *vlumeclautxtview;
+@property (strong, nonatomic) IBOutlet UITableView *vlumetable;
+@property (strong, nonatomic) IBOutlet UIView *volumetitle;
+
+/*labor*/
+
+@property (strong, nonatomic) IBOutlet UIView *labrview;
+@property (strong, nonatomic) IBOutlet UITextView *lbrclaustxtview;
+@property (strong, nonatomic) IBOutlet UITextField *weekhrstxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *subcntrctbtn;
+@property (strong, nonatomic) IBOutlet UIButton *overtimechecklbl;
+@property (strong, nonatomic) IBOutlet UIButton *minimumchecklbl;
+@property (strong, nonatomic) IBOutlet UIButton *holidayschecklbl;
+
+- (IBAction)overcheckbtn:(id)sender;
+- (IBAction)minimumhrsbtn:(id)sender;
+- (IBAction)holidaysbtn:(id)sender;
+
+/*Equipment View*/
+@property (strong, nonatomic) IBOutlet UIView *equpview;
+
+@property (strong, nonatomic) IBOutlet UITextView *equpclause;
+@property (strong, nonatomic) IBOutlet UITableView *cfetable;
+@property (strong, nonatomic) IBOutlet UITableView *tretable;
+@property (strong, nonatomic) IBOutlet UIButton *thirdequbtnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *conequpbtnlbl;
+
+- (IBAction)conequpbtn:(id)sender;
+- (IBAction)thirdequpbtn:(id)sender;
+
+/*Otherterms view*/
+@property (strong, nonatomic) IBOutlet UIView *othrtrmsview;
+@property (strong, nonatomic) IBOutlet UITextView *othrtermsclause;
+
+@property (strong, nonatomic) IBOutlet UIButton *swpbtn;
+@property (strong, nonatomic) IBOutlet UIButton *consumblebtn;
+@property (strong, nonatomic) IBOutlet UIButton *smalltoolbillingbtn;
+
+- (IBAction)shopcheckbtn:(id)sender;
+- (IBAction)consublebtn:(id)sender;
+- (IBAction)smalltoolbtn:(id)sender;
+
+/*markup view*/
+
+
+@property (strong, nonatomic) IBOutlet UIView *markupview;
+@property (strong, nonatomic) IBOutlet UIButton *markupbtnlbl;
+@property (strong, nonatomic) IBOutlet UITextField *overtimetextfield;
+@property (strong, nonatomic) IBOutlet UITextField *premiumtimetxtfld;
+- (IBAction)selectmarkupbtn:(id)sender;
+/*base view */
+@property (strong, nonatomic) IBOutlet UIView *baseview;
+@property (strong, nonatomic) IBOutlet UIView *basetitle;
+@property (strong, nonatomic) IBOutlet UITableView *basetable;
+/*fuel view*/
+@property (strong, nonatomic) IBOutlet UIView *fuelview;
+@property (strong, nonatomic) IBOutlet UIView *fueltitle;
+@property (strong, nonatomic) IBOutlet UITableView *fueltable;
+
+/*non fuel view*/
+@property (strong, nonatomic) IBOutlet UIView *nonfuelview;
+
+@property (strong, nonatomic) IBOutlet UIView *nonfueltitle;
+
+@property (strong, nonatomic) IBOutlet UITableView *nonfueltable;
+
+/*smrview */
+@property (strong, nonatomic) IBOutlet UIView *smrview;
+@property (strong, nonatomic) IBOutlet UIView *smrtitle;
+
+@property (strong, nonatomic) IBOutlet UITableView *smrtable;
+
+/*other rates */
+
+@property (strong, nonatomic) IBOutlet UIView *othrratesview;
+@property (strong, nonatomic) IBOutlet UITextField *temtxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *leasemarkuptxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *rentalmarkuptxtfld;
+@property (strong, nonatomic) IBOutlet UITextField *materialpurchasetxtfld;
+
+/*delivery rate*/
+@property (strong, nonatomic) IBOutlet UIView *deliveryview;
+@property (strong, nonatomic) IBOutlet UITextView *deliveryclausetxtview;
+/*srs view*/
+@property (strong, nonatomic) IBOutlet UIView *srsview;
+@property (strong, nonatomic) IBOutlet UITextView *srstxtview;
+/*documnt view */
+@property (strong, nonatomic) IBOutlet UIView *doctitle;
+@property (strong, nonatomic) IBOutlet UITableView *docutable;
+
+@property (strong, nonatomic) IBOutlet UIView *docuview;
+
+
 @end
--- Newproject/MangmntdetailsViewController.m
+++ Newproject/MangmntdetailsViewController.m
@@ -27,6 +27,56 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [[self.clausetxtview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.clausetxtview layer] setBorderWidth:3];
+    [[self.clausetxtview layer] setCornerRadius:10];
+     self.volumetitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    [[self.vlumeclautxtview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.vlumeclautxtview layer] setBorderWidth:3];
+    [[self.vlumeclautxtview layer] setCornerRadius:10];
+    self.vlumetable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.vlumetable.layer.borderWidth=2.0f;
+    
+    [[self.lbrclaustxtview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.lbrclaustxtview layer] setBorderWidth:3];
+    [[self.lbrclaustxtview layer] setCornerRadius:10];
+    [[self.labrview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.labrview layer] setBorderWidth:2];
+    [[self.labrview layer] setCornerRadius:10];
+
+    [[self.equpclause layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.equpclause layer] setBorderWidth:3];
+    [[self.equpclause layer] setCornerRadius:10];
+    self.cfetable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.cfetable.layer.borderWidth=2.0f;
+    self.tretable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.tretable.layer.borderWidth=2.0f;
+    [[self.othrtermsclause layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.othrtermsclause layer] setBorderWidth:3];
+    [[self.othrtermsclause layer] setCornerRadius:10];
+    self.basetable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.basetable.layer.borderWidth=2.0f;
+    self.basetitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    self.fueltitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    self.fueltable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.fueltable.layer.borderWidth=2.0f;
+    self.nonfueltitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    self.nonfueltable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.nonfueltable.layer.borderWidth=2.0f;
+    self.smrtitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    self.smrtable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.smrtable.layer.borderWidth=2.0f;
+    [[self.deliveryclausetxtview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.deliveryclausetxtview layer] setBorderWidth:3];
+    [[self.deliveryclausetxtview layer] setCornerRadius:10];
+    [[self.srstxtview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.srstxtview layer] setBorderWidth:3];
+    [[self.srstxtview layer] setCornerRadius:10];
+    self.doctitle.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    self.docutable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
+    self.docutable.layer.borderWidth=2.0f;
+
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +85,299 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)paymntbtn:(id)sender {
+    _paymentview.hidden=NO;
+     _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+       _equpview.hidden=YES;
+      _othrtrmsview.hidden=YES;
+       _markupview.hidden=YES;
+      _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+   _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)volumebtn:(id)sender {
+    _volumeview.hidden=NO;
+    _paymentview.hidden=YES;
+  
+    _labrview.hidden=YES;
+       _equpview.hidden=YES;
+      _othrtrmsview.hidden=YES;
+       _markupview.hidden=YES;
+      _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)laborbtn:(id)sender {
+    _volumeview.hidden=YES;
+    _paymentview.hidden=YES;
+    _labrview.hidden=NO;
+     _equpview.hidden=YES;
+      _othrtrmsview.hidden=YES;
+       _markupview.hidden=YES;
+      _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+   _smrview.hidden=YES;
+
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)equpbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=NO;
+      _othrtrmsview.hidden=YES;
+       _markupview.hidden=YES;
+      _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)othrtermsbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=NO;
+    _markupview.hidden=YES;
+      _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+
+    
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)markupbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=NO;
+     _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)basebtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=NO;
+     _fuelview.hidden=YES;
+     _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)fuelbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=NO;
+      _nonfuelview.hidden=YES;
+       _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+}
+
+- (IBAction)nonfuelbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=NO;
+     _smrview.hidden=YES;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)SMRbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=YES;
+    _smrview.hidden=NO;
+     _othrratesview.hidden=YES;
+      _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+
+}
+
+- (IBAction)Orbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=YES;
+    _smrview.hidden=YES;
+    _othrratesview.hidden=NO;
+    _deliveryview.hidden=YES;
+
+    _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)deliveryratebtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=YES;
+    _smrview.hidden=YES;
+    _othrratesview.hidden=YES;
+    _deliveryview.hidden=NO;
+     _srsview.hidden=YES;
+    _docuview.hidden=YES;
+
+
+}
+
+- (IBAction)srsbtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=YES;
+    _smrview.hidden=YES;
+    _othrratesview.hidden=YES;
+    _deliveryview.hidden=YES;
+    _srsview.hidden=NO;
+     _docuview.hidden=YES;
+
+}
+
+- (IBAction)docubtn:(id)sender {
+    _paymentview.hidden=YES;
+    _volumeview.hidden=YES;
+    _labrview.hidden=YES;
+    _equpview.hidden=YES;
+    _othrtrmsview.hidden=YES;
+    _markupview.hidden=YES;
+    _baseview.hidden=YES;
+    _fuelview.hidden=YES;
+    _nonfuelview.hidden=YES;
+    _smrview.hidden=YES;
+    _othrratesview.hidden=YES;
+    _deliveryview.hidden=YES;
+    _srsview.hidden=YES;
+    _docuview.hidden=NO;
+
+}
+
+
+/*btn outlets*/
+- (IBAction)overcheckbtn:(id)sender {
+}
+
+- (IBAction)minimumhrsbtn:(id)sender {
+}
+
+- (IBAction)holidaysbtn:(id)sender {
+}
+- (IBAction)conequpbtn:(id)sender {
+}
+
+- (IBAction)thirdequpbtn:(id)sender {
+}
+- (IBAction)shopcheckbtn:(id)sender {
+}
+
+- (IBAction)consublebtn:(id)sender {
+}
+
+- (IBAction)smalltoolbtn:(id)sender {
+}
+- (IBAction)selectmarkupbtn:(id)sender {
+}
 @end
