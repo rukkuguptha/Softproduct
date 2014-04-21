bf3c7a18fccc0e030dad66adb0f6f14d37d1c225
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-04-21 17:39:27 +0530

    xcdfd

--- Newproject/DetailplanViewController.m
+++ Newproject/DetailplanViewController.m
@@ -78,6 +78,7 @@
     self.generalworkctrlr=[[GeneralViewController alloc]initWithNibName:@"GeneralViewController" bundle:nil];
     _generalworkctrlr.Planid=_planid;
     _generalworkctrlr.genralid=gmodel.gid;
+    
     self.generalworkctrlr.modalPresentationStyle=UIModalPresentationPageSheet;
     [self presentViewController:_generalworkctrlr
                        animated:YES completion:NULL];
