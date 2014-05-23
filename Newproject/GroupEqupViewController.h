b8b3ba2574f34b3782e14810e5b5d61f9e8d01dc
Riagms <riyariya44@gmail.com>
2014-05-23 11:17:14 +0530

    cmmtd

---
+++ Newproject/GroupEqupViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupEqupViewController.h
+//  Newproject
+//
+//  Created by GMSIndia1 on 5/23/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupEqupViewController : UIViewController
+
+@end
---
+++ Newproject/GroupEqupViewController.m
@@ -0,0 +1,38 @@
+//
+//  GroupEqupViewController.m
+//  Newproject
+//
+//  Created by GMSIndia1 on 5/23/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import "GroupEqupViewController.h"
+
+@interface GroupEqupViewController ()
+
+@end
+
+@implementation GroupEqupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
