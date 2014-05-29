7fd9655116f4fb6ba504df276b8c564eddcc5c7e
Riagms <riyariya44@gmail.com>
2014-05-29 10:27:22 +0530

    cmmtd

---
+++ Newproject/Eventmdl.h
@@ -0,0 +1,18 @@
+//
+//  Eventmdl.h
+//  Newproject
+//
+//  Created by GMSIndia1 on 5/29/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Eventmdl : NSObject
+
+@property(strong,nonatomic)NSString*startdate;
+@property(strong,nonatomic)NSString*enddate;
+@property(strong,nonatomic)NSString*Title;
+@property(strong,nonatomic)NSString*sum1;
+
+@end
