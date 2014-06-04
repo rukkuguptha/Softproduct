6ed954de28ae17b56d1286c931e1282d071e5560
Riagms <riyariya44@gmail.com>
2014-06-04 10:56:09 +0530

    cmmtd

--- Newproject/EsttileViewController.m
+++ Newproject/EsttileViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
     self.view.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:226/255.0f blue:226/255.0f alpha:1.0f];
     
    
