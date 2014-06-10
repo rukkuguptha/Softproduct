6f80e4a2a9104f697795bd31c7b379044c80cb37
Riagms <riyariya44@gmail.com>
2014-06-10 15:15:21 +0530

    cmmtd

--- Newproject/EqothersViewController.m
+++ Newproject/EqothersViewController.m
@@ -30,14 +30,445 @@
     _eqtable.layer.borderWidth=3.0;
     _eqtable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:250.0/255.0f alpha:1.0f].CGColor;
     _titleview.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:250.0/255.0f alpha:1.0f];
+    _Optionarray=[[NSMutableArray alloc]initWithObjects:@"Job Number",@"Item Name",@"Type",@"Date", nil];
 
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self EquipmentandOthersSelect];
+    _mydict=[[NSMutableDictionary alloc]init];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark-Popover
+-(void)createpopover{
+    UIViewController* popoverContent = [[UIViewController alloc]
+                                        init];
+    
+    UIView* popoverView = [[UIView alloc]
+                           initWithFrame:CGRectMake(0, 0, 200, 120)];
+    
+    popoverView.backgroundColor = [UIColor whiteColor];
+    _popOverTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, 200, 120)];
+    
+    _popOverTableView.delegate=(id)self;
+    _popOverTableView.dataSource=(id)self;
+    _popOverTableView.rowHeight= 32;
+    _popOverTableView.separatorStyle=UITableViewCellSeparatorStyleSingleLine;
+    
+    
+    // CGRect rect = frame;
+    [popoverView addSubview:_popOverTableView];
+    popoverContent.view = popoverView;
+    
+    //resize the popover view shown
+    //in the current view to the view's size
+    popoverContent.contentSizeForViewInPopover = CGSizeMake(200, 120);
+    
+    //create a popover controller
+    
+    self.popOverController = [[UIPopoverController alloc]
+                              initWithContentViewController:popoverContent];
+    
+    //
+    //    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    //    CGRect rect=CGRectMake(cell.bounds.origin.x+90, cell.bounds.origin.y+10, 50, 30);
+    //    [self.popOverController presentPopoverFromRect:_disclsurelbl.bounds inView:self.view permittedArrowDirections:nil animated:YES];
+    
+    
+    [self.popOverController presentPopoverFromRect:_optionbtnlbl.frame
+                                            inView:self.scroll
+                          permittedArrowDirections:UIPopoverArrowDirectionUp
+                                          animated:YES];
+    
+    
+    
+      
+    
+    
+}
+
+#pragma mark-Web Service
+-(void)EquipmentandOthersSelect{
+      recordResults = FALSE;
+    NSString *soapMessage;
+    
+    
+    soapMessage = [NSString stringWithFormat:
+                   
+                   @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                   "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                   
+                   
+                   "<soap:Body>\n"
+                   
+                   "<EquipmentandOthersSelect xmlns=\"http://ios.kontract360.com/\">\n"
+                   
+                   "</EquipmentandOthersSelect>\n"
+                   "</soap:Body>\n"
+                   "</soap:Envelope>\n"];
+    NSLog(@"soapmsg%@",soapMessage);
+    
+    
+     NSURL *url = [NSURL URLWithString:@"http://192.168.0.100/service.asmx"];
+  //  NSURL *url = [NSURL URLWithString:@"http://test3.kontract360.com/service.asmx"];;
+    
+    NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:url];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"%d", [soapMessage length]];
+    
+    [theRequest addValue: @"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    [theRequest addValue: @"http://ios.kontract360.com/EquipmentandOthersSelect" forHTTPHeaderField:@"Soapaction"];
+    [theRequest addValue: msgLength forHTTPHeaderField:@"Content-Length"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody: [soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    
+    if( theConnection )
+    {
+        _webData = [NSMutableData data];
+    }
+    else
+    {
+        ////NSLog(@"theConnection is NULL");
+    }
+    
+}
+
+
+
+#pragma mark - Connection
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+	[_webData setLength: 0];
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+   	[_webData appendData:data];
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    UIAlertView *  Alert=[[UIAlertView alloc]initWithTitle:nil message:@"ERROR with the Connection" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    
+    [Alert show];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"DONE. Received Bytes: %d", [_webData length]);
+	NSString *theXML = [[NSString alloc] initWithBytes: [_webData mutableBytes] length:[_webData length] encoding:NSUTF8StringEncoding];
+	NSLog(@"xml===== %@",theXML);
+    
+    
+	
+	if( _xmlParser )
+	{
+		
+	}
+	
+	_xmlParser = [[NSXMLParser alloc] initWithData: _webData];
+	[_xmlParser setDelegate:(id)self];
+	[_xmlParser setShouldResolveExternalEntities: YES];
+    NSDictionary *dictionary=[_xmlParser dictionaryWithValuesForKeys:[[NSArray alloc]initWithObjects:@"EquipmentandOthersSelectResponse",@"Table", nil]];
+    
+                                     NSLog(@"dictioanry is %@",dictionary);
+
+	[_xmlParser parse];
+    
+    [_eqtable reloadData];
+    
+       }
+#pragma mark-xml parser
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *) namespaceURI qualifiedName:(NSString *)qName
+   attributes: (NSDictionary *)attributeDict{
+    if([elementName isEqualToString:@"EquipmentandOthersSelectResponse"])
+    {
+        _equipmntarray=[[NSMutableArray alloc]init];
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"EntryId"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"JobNumber"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ItemCode"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ItemName"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    if([elementName isEqualToString:@"Type"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"Date"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    if([elementName isEqualToString:@"OrderedQuantity"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"StockQuantity"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    
+    if([elementName isEqualToString:@"DeliveredQty"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"ReceivedQty"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"TotalQtySendBack"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+    if([elementName isEqualToString:@"QtyReceivedBack"])
+    {
+        
+        
+        if(!_soapResults)
+        {
+            _soapResults = [[NSMutableString alloc] init];
+        }
+        recordResults = TRUE;
+    }
+
+}
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    
+    
+    
+	if( recordResults )
+        
+	{
+        
+        
+		[_soapResults appendString: string];
+    }
+}
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+      if([elementName isEqualToString:@"EntryId"])
+    {
+        
+        
+        _equpmdl=[[EqOthersMdl alloc]init];
+        recordResults = FALSE;
+        _equpmdl.entryid=_soapResults;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"JobNumber"])
+    {
+        
+        
+        recordResults = FALSE;
+        _equpmdl.jobnumber=_soapResults;
+
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"ItemCode"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.itemcode=_soapResults;
+        _soapResults = nil;
+        
+    }
+    if([elementName isEqualToString:@"ItemName"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.itemname=_soapResults;
+        _soapResults = nil;
+    }
+    
+    if([elementName isEqualToString:@"Type"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.type=_soapResults;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"Date"])
+    {
+        
+        recordResults = FALSE;
+        NSArray *dateArray=[[NSArray alloc]init];
+        dateArray=[_soapResults componentsSeparatedByString:@"T"];
+        NSString *date1 =[dateArray objectAtIndex:0];
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"yyyy-MM-dd"];
+        NSDate *dates = [dateFormat dateFromString:date1];
+        [dateFormat setDateFormat:@"MM-dd-yyy"];
+        NSString *myFormattedDate = [dateFormat stringFromDate:dates];
+        
+      
+           _equpmdl.itemdate=myFormattedDate;
+        _soapResults = nil;
+    }
+    
+    if([elementName isEqualToString:@"OrderedQuantity"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.OrderedQuantity=_soapResults;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"StockQuantity"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.StockQuantity=_soapResults;
+        _soapResults = nil;
+    }
+    
+    if([elementName isEqualToString:@"DeliveredQty"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.DeliveredQty=_soapResults;
+        _soapResults = nil;
+        
+    }
+    if([elementName isEqualToString:@"ReceivedQty"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.ReceivedQty=_soapResults;
+        _soapResults = nil;
+    }
+    if([elementName isEqualToString:@"TotalQtySendBack"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.TotalQtySendBack=_soapResults;
+        _soapResults = nil;
+        
+    }
+    if([elementName isEqualToString:@"QtyReceivedBack"])
+    {
+        
+        
+        recordResults = FALSE;
+           _equpmdl.QtyReceivedBack=_soapResults;
+        [_equipmntarray addObject:_equpmdl];
+             _soapResults = nil;
+    }
+    
+    
+}
+- (void)parserDidEndDocument:(NSXMLParser *)parser{
+    NSArray*newarray=[_equipmntarray mutableCopy];
+    for (int i = 0; i<[_equipmntarray count]; i++) {
+        
+        EqOthersMdl*eqmdl=(EqOthersMdl *)[_equipmntarray objectAtIndex:i];
+    NSSortDescriptor *sortdescpt=[[NSSortDescriptor alloc]initWithKey:eqmdl.itemdate ascending:NO];
+    NSArray*sortedarray=[[NSArray alloc]initWithObjects:sortdescpt, nil];
+          NSLog(@"array%@",[sortedarray objectAtIndex:0]);
+   NSArray*array=[newarray sortedArrayUsingDescriptors:sortedarray];
+  
+        NSLog(@"array%@",array);
+ 
+    }
+    NSLog(@"arrayuuuu");
+}
+
 #pragma mark-Tableview
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
@@ -51,7 +482,15 @@
 {
     
     // Return the number of rows in the section.
-    return 3;
+    
+    if (tableView==_popOverTableView) {
+        return [_Optionarray count];
+    }
+    else{
+        return 3;
+    }
+    return YES;
+
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -60,26 +499,80 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        if (tableView==_eqtable) {
+            
         
         [[NSBundle mainBundle]loadNibNamed:@"Eqotherscell" owner:self options:nil];
         cell=_eqcell;
+        }
+    }
+    cell.textLabel.font=[UIFont fontWithName:@"Helvetica Neue" size:12];
+
+    if (tableView==_popOverTableView) {
+        
+        cell.textLabel.text=[_Optionarray objectAtIndex:indexPath.row];
     }
     
-    
-//    Mdladdress *addmdl=(Mdladdress *)[_addesslistarray objectAtIndex:indexPath.row];
-//    _headerlbl=(UILabel *)[cell viewWithTag:1];
-//    _headerlbl.text=addmdl.header;
-//    _addresslbl=(UILabel *)[cell viewWithTag:2];
-//    _addresslbl.text=addmdl.address;
-    
-    
+    if (tableView==_eqtable) {
+    EqOthersMdl*eqmmdl=(EqOthersMdl *)[_equipmntarray objectAtIndex:indexPath.row];
+    _jobnumbrlbl=(UILabel *)[cell viewWithTag:1];
+    _jobnumbrlbl.text=eqmmdl.jobnumber;
+    _itmcodelbl=(UILabel *)[cell viewWithTag:2];
+    _itmcodelbl.text=eqmmdl.itemcode;
+    _itemnamelbl=(UILabel *)[cell viewWithTag:3];
+    _itemnamelbl.text=eqmmdl.itemname;
+    _typelbl=(UILabel *)[cell viewWithTag:4];
+    _typelbl.text=eqmmdl.type;
+    _datelbl=(UILabel *)[cell viewWithTag:5];
+    _datelbl.text=eqmmdl.itemdate;
+    _orderdlbl=(UILabel *)[cell viewWithTag:6];
+    _orderdlbl.text=eqmmdl.OrderedQuantity;
+    _stocklbl=(UILabel *)[cell viewWithTag:7];
+    _stocklbl.text=eqmmdl.StockQuantity;
+    }
+
     return cell;
     
     
 }
+#pragma mark-Tableview Datasource
+-(void)tableView: (UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (tableView==_popOverTableView) {
+        
+        [_optionbtnlbl setTitle:[_Optionarray objectAtIndex:indexPath.row] forState:UIControlStateNormal ];
+       }
+    [self.popOverController dismissPopoverAnimated:YES];
+}
+
 
 - (IBAction)clsebtn:(id)sender {
    
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)selectbtn:(id)sender {
+   
+}
+
+- (IBAction)checkbtn:(id)sender {
+    btnclck++;
+    if (btnclck%2!=0) {
+        [_checkbtnlbl setImage:[UIImage imageNamed:@"cb_mono_on"] forState:UIControlStateNormal];
+        _selectbtnlbl.enabled=YES;
+    }
+    else
+    {
+        [_checkbtnlbl setImage:[UIImage imageNamed:@"cb_mono_off"] forState:UIControlStateNormal];
+          _selectbtnlbl.enabled=NO;
+    }
+
+}
+
+- (IBAction)optnbtn:(id)sender {
+    [self createpopover];
+    [_popOverTableView reloadData];
+}
+
+- (IBAction)showbtn:(id)sender {
 }
 @end
