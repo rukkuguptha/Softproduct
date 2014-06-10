7e5858e32d87c9a12817a51f17faa5b9303c8a2f
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-06-10 09:45:35 +0530

    cmt

--- Newproject/LabourViewController.h
+++ Newproject/LabourViewController.h
@@ -10,10 +10,20 @@
 
 @interface LabourViewController : UIViewController
 
-
+@property(strong,nonatomic)IBOutlet UIView *titleview;
 @property(strong,nonatomic)IBOutlet UITableViewCell *labrcell;
+@property(strong,nonatomic)IBOutlet UITableView *labrtable;
+@property(strong,nonatomic)IBOutlet UILabel *totalquantitylabel;
+
+/*cell Labels*/
+@property(strong,nonatomic)IBOutlet UILabel *joblbl;
+@property(strong,nonatomic)IBOutlet UILabel *itemcodelbl;
+@property(strong,nonatomic)IBOutlet UILabel *deslbl;
+@property(strong,nonatomic)IBOutlet UILabel *datelbl;
+@property(strong,nonatomic)IBOutlet UILabel *quantitylbl;
 
 
+@property(strong,nonatomic)NSMutableArray *labourarray;
 
 - (IBAction)clsebtn:(id)sender;
 @end
--- Newproject/LabourViewController.m
+++ Newproject/LabourViewController.m
@@ -26,6 +26,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:226/255.0f blue:226/255.0f alpha:1.0f];
+  
+    // Do any additional setup after loading the view from its nib.
+    _titleview.backgroundColor = [UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f];
+    
+    _labrtable.layer.borderWidth=2.0f;
+    _labrtable.layer.borderColor=[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor;
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -39,5 +46,68 @@
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
+#pragma mark-Tableview
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    // Return the number of sections.
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+    
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellidentifier=@"mycell";
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:cellidentifier];
+    if (cell==nil) {
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellidentifier];
+        
+        if(tableView==_labrtable)
+        {
+            [[NSBundle mainBundle]loadNibNamed:@"Lbrcuscell" owner:self options:nil];
+            cell=_labrcell;
+        }
+    }
+    
+    return cell;
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    
+    
+    
+}
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    //alternating cell back ground color
+    if(tableView==_labrtable)
+    {
+        if (indexPath.row%2 == 0) {
+            [cell setBackgroundColor:[UIColor whiteColor]];
+            
+        }else
+        {
+            
+            //[cell setBackgroundColor:[UIColor colorWithRed:247.0/255.0f green:247.0/255.0f blue:247.0/255.0f alpha:1.0f]];
+            [cell setBackgroundColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f]];
+            
+            
+        }
+    }
+}
+
+
+
+
 
 @end
