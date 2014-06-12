af80b0e324e51b8040847f8374240790f8cbf5f9
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-06-12 15:31:09 +0530

    site in and out added

---
+++ Newproject/InOutSiteViewController.m
@@ -0,0 +1,43 @@
+//
+//  InOutSiteViewController.m
+//  Newproject
+//
+//  Created by GMSIndia 2 on 12/06/14.
+//  Copyright (c) 2014 GMSIndia1. All rights reserved.
+//
+
+#import "InOutSiteViewController.h"
+
+@interface InOutSiteViewController ()
+
+@end
+
+@implementation InOutSiteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(IBAction)closethepage:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+@end
