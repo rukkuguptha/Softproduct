68349c7ee80d47e86d6d89aad179a49cfd87a5cd
GMSIndia 2 <rukminikr@GMSIndia-2.local>
2014-06-13 10:11:39 +0530

    committed

--- Newproject/InOutSiteViewController.h
+++ Newproject/InOutSiteViewController.h
@@ -9,9 +9,47 @@
 #import <UIKit/UIKit.h>
 
 @interface InOutSiteViewController : UIViewController
+{
+    NSInteger confirmcheck;
+     NSInteger outconfirmcheck;
+}
 
 
 -(IBAction)closethepage:(id)sender;
 
+
+/*IBOutlets in Sitein*/
+@property(strong,nonatomic)IBOutlet UITextField *jobsitetxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *itemcodetxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *orderedqtytxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *receivedqtytxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *qtyrcvdnwtxtfld;
+@property(strong,nonatomic)IBOutlet UITextView *notetextview;
+@property(strong,nonatomic)IBOutlet UIButton *cnfrmbtn;
+
+-(IBAction)confirmcheckaction:(id)sender;
+
+
+/*IBOutlets in Siteout*/
+@property(strong,nonatomic)IBOutlet UITextField *outjobsitetxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *outitemcodetxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *outorderedqtytxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *outreceivedqtytxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *outtotalqtysendtxtfld;
+@property(strong,nonatomic)IBOutlet UITextField *outqtysendbacktxtfld;
+@property(strong,nonatomic)IBOutlet UITextView *outnotetextview;
+@property(strong,nonatomic)IBOutlet UIButton *outcnfrmbtn;
+@property(strong,nonatomic)IBOutlet UIButton *tojobbtn;
+
+-(IBAction)outconfirmcheckaction:(id)sender;
+-(IBAction)tojobsiteaction:(id)sender;
+
+
+
+@property(strong,nonatomic)IBOutlet UITableView *popovertableview;
+@property(strong,nonatomic)UIPopoverController *popovercontroller;
+
+
+@property(strong,nonatomic)NSMutableArray *inoutarray;
 
 @end
--- Newproject/InOutSiteViewController.m
+++ Newproject/InOutSiteViewController.m
@@ -26,6 +26,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+  self.view.backgroundColor=[UIColor colorWithRed:234.0/255.0f green:226/255.0f blue:226/255.0f alpha:1.0f];
+    [[self.notetextview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.notetextview layer] setBorderWidth:2];
+    [[self.notetextview layer] setCornerRadius:10];
+    
+    [[self.outnotetextview layer] setBorderColor:[UIColor colorWithRed:234.0/255.0f green:244.0/255.0f blue:249.0/255.0f alpha:1.0f].CGColor];
+    [[self.outnotetextview layer] setBorderWidth:2];
+    [[self.outnotetextview layer] setCornerRadius:10];
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -38,6 +46,135 @@
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
+-(IBAction)confirmcheckaction:(id)sender
+{
+    if(confirmcheck==0)
+    {
+        
+        [_cnfrmbtn setImage:[UIImage imageNamed:@"cb_mono_on"] forState:UIControlStateNormal];
+        
+        
+        confirmcheck=1;
+        
+        
+    }
+    else
+    {
+        [_cnfrmbtn setImage:[UIImage imageNamed:@"cb_mono_off"] forState:UIControlStateNormal];
+        confirmcheck=0;
+        
+    }
+
+}
+-(IBAction)tojobsiteaction:(id)sender
+{
+    UIViewController *popovercontent=[[UIViewController alloc]init];
+    UIView *popoverview=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 210, 120)];
+    popoverview.backgroundColor=[UIColor whiteColor];
+    _popovertableview=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, 210, 120)];
+    _popovertableview.delegate=(id)self;
+    _popovertableview.dataSource=(id)self;
+    _popovertableview.rowHeight=32;
+    _popovertableview.separatorStyle=UITableViewCellSeparatorStyleSingleLine;
+    [popoverview addSubview:_popovertableview];
+    popovercontent.view=popoverview;
+    popovercontent.contentSizeForViewInPopover=CGSizeMake(210, 120);
+    self.popovercontroller=[[UIPopoverController alloc]initWithContentViewController:popovercontent];
+    [self.popovercontroller presentPopoverFromRect:_tojobbtn.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+    
+    
+ 
+}
+-(IBAction)outconfirmcheckaction:(id)sender
+{
+    if(outconfirmcheck==0)
+    {
+        
+        [_outcnfrmbtn setImage:[UIImage imageNamed:@"cb_mono_on"] forState:UIControlStateNormal];
+        
+        
+        outconfirmcheck=1;
+        
+        
+    }
+    else
+    {
+        [_outcnfrmbtn setImage:[UIImage imageNamed:@"cb_mono_off"] forState:UIControlStateNormal];
+        outconfirmcheck=0;
+        
+    }
+
+}
+#pragma mark-Tableview
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    // Return the number of sections.
+    return 1;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    // Return the number of rows in the section.
+    if(tableView==_popovertableview)
+        
+    {
+        return 5;
+    }
+    
+    return YES;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"mycell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    if(tableView==_popovertableview)
+    {  cell.textLabel.font = [UIFont fontWithName:@"Helvetica Neue Light" size:12];
+        cell.textLabel.font = [UIFont systemFontOfSize:12.0];
+      
+                //cell.textLabel.text=[_statearray objectAtIndex:indexPath.row];
+        
+                
+        
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return cell;
+    
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    if (tableView==_popovertableview) {
+        
+        
+       
+        
+        
+        
+    }
+    [self.popovercontroller dismissPopoverAnimated:YES];
+    
+}
+
+
+
 
 
 @end
