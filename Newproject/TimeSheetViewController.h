a2b210101eba495b00943fc19950c928c0eddc91
Riagms <riyariya44@gmail.com>
2014-06-30 12:52:36 +0530

    c

--- Newproject/TimeSheetViewController.h
+++ Newproject/TimeSheetViewController.h
@@ -9,6 +9,57 @@
 #import <UIKit/UIKit.h>
 
 @interface TimeSheetViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *view1;
+@property (strong, nonatomic) IBOutlet UIView *view2;
+
+
+
+
+@property (strong, nonatomic) IBOutlet UIButton *allemplyescheckbtnlbl;
+
+@property (strong, nonatomic) IBOutlet UIButton *foremancheckbtnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *foremanbtnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *time1checkbtnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *time2checkbtnlbl;
+@property (strong, nonatomic) IBOutlet UITextField *time1txtfld;
+@property (strong, nonatomic) IBOutlet UITextField *time2txtfld;
+@property (strong, nonatomic) IBOutlet UIButton *phase1btnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *sequence1btnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *tagnumber1btnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *phase2btnlbl;
+@property (strong, nonatomic) IBOutlet UIButton *sequence2btnlbl;
+
+@property (strong, nonatomic) IBOutlet UIButton *tagnumber2btnlbl;
+
+
+- (IBAction)allemplysbtn:(id)sender;
+- (IBAction)foremanbtn:(id)sender;
+- (IBAction)formanselctn:(id)sender;
+- (IBAction)time1btn:(id)sender;
+- (IBAction)time2btn:(id)sender;
+- (IBAction)phase1btn:(id)sender;
+- (IBAction)sequence1btn:(id)sender;
+- (IBAction)tagnumber1btn:(id)sender;
+- (IBAction)phase2btn:(id)sender;
+- (IBAction)sequence2btn:(id)sender;
+- (IBAction)tagnumber2btn:(id)sender;
+
+
+- (IBAction)laborbtn:(id)sender;
+- (IBAction)equpbtn:(id)sender;
+- (IBAction)fleetbtn:(id)sender;
+- (IBAction)tpfbtn:(id)sender;
+
+- (IBAction)tpebtn:(id)sender;
+- (IBAction)materialsbtn:(id)sender;
+- (IBAction)consumblesbtn:(id)sender;
+- (IBAction)othrsbtn:(id)sender;
+
+
+
+- (IBAction)applybtn:(id)sender;
+
+
 - (IBAction)clsebtn:(id)sender;
 
 @end
--- Newproject/TimeSheetViewController.m
+++ Newproject/TimeSheetViewController.m
@@ -35,6 +35,94 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)allemplysbtn:(id)sender {
+}
+
+- (IBAction)foremanbtn:(id)sender {
+}
+
+- (IBAction)formanselctn:(id)sender {
+}
+
+- (IBAction)time1btn:(id)sender {
+}
+
+- (IBAction)time2btn:(id)sender {
+}
+
+- (IBAction)phase1btn:(id)sender {
+}
+
+- (IBAction)sequence1btn:(id)sender {
+}
+
+- (IBAction)tagnumber1btn:(id)sender {
+}
+
+- (IBAction)phase2btn:(id)sender {
+}
+
+- (IBAction)sequence2btn:(id)sender {
+}
+
+- (IBAction)tagnumber2btn:(id)sender {
+}
+
+
+#pragma mark-Barbutton Actions
+
+- (IBAction)laborbtn:(id)sender {
+    _view1.hidden=NO;
+     _view2.hidden=YES;
+}
+
+- (IBAction)equpbtn:(id)sender {
+    _view1.hidden=NO;
+    _view2.hidden=YES;
+
+}
+
+- (IBAction)fleetbtn:(id)sender {
+    _view1.hidden=NO;
+    _view2.hidden=YES;
+
+}
+
+- (IBAction)tpfbtn:(id)sender {
+    _view1.hidden=NO;
+    _view2.hidden=YES;
+
+}
+
+- (IBAction)tpebtn:(id)sender {
+    _view1.hidden=NO;
+    _view2.hidden=YES;
+
+}
+
+- (IBAction)materialsbtn:(id)sender {
+    _view2.hidden=NO;
+    _view1.hidden=YES;
+
+}
+
+- (IBAction)consumblesbtn:(id)sender {
+    _view2.hidden=NO;
+    _view1.hidden=YES;
+}
+
+- (IBAction)othrsbtn:(id)sender {
+    _view2.hidden=NO;
+    _view1.hidden=YES;
+}
+
+- (IBAction)applybtn:(id)sender {
+    _view2.hidden=NO;
+    _view1.hidden=YES;
+}
+
 - (IBAction)clsebtn:(id)sender {
+    _view2.hidden=NO;
+    _view1.hidden=YES;
 }
 @end
